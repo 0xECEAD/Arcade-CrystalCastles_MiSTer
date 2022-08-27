d72190facb4c0e22de57d4aad5453aef679e2dfa
Enceladus <enc@seynen.net>
2022-08-27 19:32:11 +0200

    Rename Template files

---
+++ rtl/ccastles.v
@@ -0,0 +1,75 @@
+module ccastles
+(
+	input         clk,
+	input         reset,
+	
+	input         pal,
+	input         scandouble,
+
+	output reg    ce_pix,
+
+	output reg    HBlank,
+	output reg    HSync,
+	output reg    VBlank,
+	output reg    VSync,
+
+	output  [7:0] video
+);
+
+reg   [9:0] hc;
+reg   [9:0] vc;
+
+
+always @(posedge clk) begin
+	if(scandouble) ce_pix <= 1;
+		else ce_pix <= ~ce_pix;
+
+	if(reset) begin
+		hc <= 0;
+		vc <= 0;
+	end
+	else if(ce_pix) begin
+		if(hc == 637) begin
+			hc <= 0;
+			if(vc == (pal ? (scandouble ? 623 : 311) : (scandouble ? 523 : 261))) begin 
+				vc <= 0;
+			end else begin
+				vc <= vc + 1'd1;
+			end
+		end else begin
+			hc <= hc + 1'd1;
+		end
+
+	end
+end
+
+always @(posedge clk) begin
+	if (hc == 529) HBlank <= 1;
+		else if (hc == 0) HBlank <= 0;
+
+	if (hc == 544) begin
+		HSync <= 1;
+
+		if(pal) begin
+			if(vc == (scandouble ? 609 : 304)) VSync <= 1;
+				else if (vc == (scandouble ? 617 : 308)) VSync <= 0;
+
+			if(vc == (scandouble ? 601 : 300)) VBlank <= 1;
+				else if (vc == 0) VBlank <= 0;
+		end
+		else begin
+			if(vc == (scandouble ? 490 : 245)) VSync <= 1;
+				else if (vc == (scandouble ? 496 : 248)) VSync <= 0;
+
+			if(vc == (scandouble ? 480 : 240)) VBlank <= 1;
+				else if (vc == 0) VBlank <= 0;
+		end
+	end
+	
+	if (hc == 590) HSync <= 0;
+end
+
+
+assign video = hc[7:0];
+
+endmodule
